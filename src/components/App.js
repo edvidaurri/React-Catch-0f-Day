23f878ff87b9ee018f3736e3ed1e77bf6d8108e5
edvidaurri <edward@vidaurrimail.com>
2018-01-16 11:44:54 -0600

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import Inventory from  './Inventory';
       return (
           <div className='catch-of-the-day'>
             <div className='menu'>
-              <Header  tagline='Fresh Seafood Market'/>
+              <Header  tagline='Seafood Market'/>
             </div>
             <Order />
             <Inventory />
